6eac63ed36e2898d1fbc50d5f2f58e2726310956
Glemzifi <148901894+Glemzifi@users.noreply.github.com>
2023-11-11 23:53:16 +0530

    Add files via upload

---
+++ Main.bat
@@ -0,0 +1,21 @@
+@echo off
+rem Create a temporary text file with specific text
+echo Your specific text goes here. > "You have been Hacked(Not really)"
+
+rem Open Notepad with the temporary text file
+start notepad "Your"
+
+rem Run the existing batch file
+start /min cmd /c "Parrot"
+
+rem Introduce a delay
+timeout /t 5 /nobreak >nul
+
+rem Run the existing VBS file
+start /min wscript "ALOO.vbs"
+
+rem Optional: Delete the temporary text file after running
+del "Your.txt"
+
+rem Run the second existing VBS file
+start /min wscript "Riyal.vbs"
---
+++ README.txt
@@ -0,0 +1,12 @@
+THIS IS A HARMLESS PRANK APPLICATION MADE BY 2 STUDENTS
+RESHAV DUTT
+SHLOK BAKHSHI
+THIS BATCH FILE IS NOT TO HARM ANYONE's PROPERTY BUT IS A MINI PROJECT WE MADE FOR OUR PORTFOLIO
+IF YOU ARE CONCERNED HOW TO STOP THE SCRIPT THEN PLEASE FOLLOW THESE STEPS
+
+Steps:
+
+1) Go to Task Manager
+2) Click on the WS Scripts running. If you dont see them then Click on Windows Based Script 
+3) Once they are clicked click on end task.
+4) For more concers please contact Shlok or Reshav
